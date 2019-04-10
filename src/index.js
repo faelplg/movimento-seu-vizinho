65d9f383cc83a42eb13224d0eb23fe97daba1a18
faelplg <faelplg@gmail.com>
2019-04-09 22:03:01 -0300

    Updating cover.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './base.scss';
 import footer from './theme/images/footerImg.jpeg';
+import cover from './theme/images/cover_seu_vizinho.jpg';
 
 /* eslint-disable no-console */
 console.log('This is a gift from @faelplg.');
 console.log('footer', footer);
+console.log('cover', cover);
 /* eslint-enable no-console */
